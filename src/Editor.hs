b3f51c4c00ad5494547a315d9285c7f04b1043a1
Allison-Turner <hermione2259@gmail.com>
2021-12-07 22:03:59 -0800

    adding new file with notes for James to make the Editor app in

---
+++ src/Editor.hs
@@ -0,0 +1,7 @@
+module Editor where
+
+--this is where James will build his editor brick app
+
+-- James' Editor should have an App{} object passed to a customMain or defaultMain
+-- an event handler for each type of keyboard control
+-- etc.
