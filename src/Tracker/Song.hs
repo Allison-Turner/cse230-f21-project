1206f3a44024008e08d7a8ad6bd77cc57c0e8872
Joey Rudek <jer5ae@virginia.edu>
2021-12-10 18:35:21 -0800

    Unit tests! They pass

--- src/Tracker/Song.hs
+++ src/Tracker/Song.hs
@@ -7,9 +7,11 @@ import Data.Aeson.TH
 import Data.Aeson
 import GHC.Generics
 import Data.Ix (Ix)
+import Test.QuickCheck
+import Test.QuickCheck.Gen
 
 -- | A pitch, encoded MIDI-style (A4 is 69)
-newtype Pitch = Pitch Int deriving (Eq, Ord, Num, Real, Enum, Ix, Integral, ToJSON, FromJSON) via Int
+newtype Pitch = Pitch Int deriving (Eq, Ord, Num, Real, Enum, Ix, Integral, ToJSON, FromJSON, Arbitrary) via Int
 
 instance Show Pitch where
   show p = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"] !! (fromIntegral p `mod` 12)
@@ -49,7 +51,6 @@ goToBeginning :: Song -> Song
 goToBeginning (Song (p:prev) curr next) = goToBeginning (Song prev p (curr:next))
 goToBeginning song = song
 
-
 -- for JSON serialization and deserialization
 $(deriveJSON defaultOptions ''Note)
 $(deriveJSON defaultOptions ''Song)
@@ -61,3 +62,23 @@ exampleSong = Song [] curr next
 
 emptySong :: Song
 emptySong = Song [] Rest []
+
+-- | Unit tests with QuickCheck
+
+instance Arbitrary Note where
+  arbitrary = maybe Rest Note <$> arbitrary
+instance Arbitrary Song where
+  arbitrary = Song <$> arbitrary <*> arbitrary <*> arbitrary
+
+-- Moving forward followed by moving backwards does nothing
+forwardAndBack song = maybe True (==song) $ forwardOneNote song >>= backOneNote
+-- Moving backwards followed by moving forward does nothing
+backAndForward song = maybe True (==song) $ backOneNote song >>= forwardOneNote
+-- Serializing and deserializing are inverses
+serde song = decode (encode song) == Just song
+-- Moving forwards in the empty song is impossible
+forwardsOfEmpty = forwardOneNote emptySong == Nothing
+-- Moving backwards in the empty song is impossible
+backOfEmpty = backOneNote emptySong == Nothing
+-- Moving backwards at the beginning is impossible
+backAtBeginning song = backOneNote (goToBeginning song) == Nothing
